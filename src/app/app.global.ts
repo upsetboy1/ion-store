2f3e918186e009b6bfe4de092bb7f1005b761e36
upsetboy1 <jawadajaz@gmail.com>
2018-11-18 15:50:47 +0500

    fixed api issues and changed the theme default color

--- src/app/app.global.ts
+++ src/app/app.global.ts
@@ -1,11 +1,11 @@
 export const App: any = {
   store           : 'Ionstore 2 - Tabs',  // change this with your app name
   ContactEmail    : 'ionicpremium@gmail.com', // change this with your email contact
-  url             : 'https://sinkingink.com/expr/', // change this with your URL (please use https, recommended)
+  url             : 'https://sinkingink.com/', // change this with your URL (please use https, recommended)
   OneSignalAppID  : 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', // change this with your onesignal api key
   GCM             : 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', // Google Project Number for Google Cloud Messaging
-  consumerKey     : 'ck_6d549a64233b7d099c3bfbaf69e7496c6252e2cc', // change this with your Consumer Key from WooCommerce
-  consumerSecret  : 'cs_a16c6069b10b71ff845feafa2fef4365166721ba', // change this with your Consumer Secret from WooCommerce
+  consumerKey     : 'ck_387f3df234f98b198e9193af132384d237d5bb45', // change this with your Consumer Key from WooCommerce
+  consumerSecret  : 'cs_571719d2e607941155817b056f690d75a922dd4c', // change this with your Consumer Secret from WooCommerce
 
   paypalSandboxClientID: 'AZjyISbp1zmOhZ0o_iAG3W2IGjlz2hvEC-8cGoQ7fXcMFN9afaRuW0X1B1PVSgkSuTQWOKqM9N4NTkOP',
   paypalLiveClientID: '', // get this from paypal developer dashboard
